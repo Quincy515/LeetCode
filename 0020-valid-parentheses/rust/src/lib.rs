a52fe1d19fe9e8ce7fb9f2f78fa3298f5d501f0d
Custer <custertian@163.com>
2021-07-26 21:20:28 +0800

    0020-valid-parentheses

--- 0020-valid-parentheses/rust/src/lib.rs
+++ 0020-valid-parentheses/rust/src/lib.rs
@@ -2,6 +2,27 @@ struct Solution;
 
 impl Solution {
   pub fn is_valid(s: String) -> bool {
+    let chars: Vec<char> = s.chars().collect();
+    // 判断是否为空字符串，空字符串视为有效字符串
+    if chars.len() == 0 { return true; }
+    let mut stack: Vec<char> = Vec::new();
+    for i in 0..chars.len() {
+      // 如果是左小括号，将右小括号入栈
+      if chars[i] == '(' { stack.push(')'); }
+      // 如果是左中括号，将右中括号入栈
+      else if chars[i] == '[' { stack.push(']'); }
+      // 如果是左大括号，将右大括号入栈
+      else if chars[i] == '{' { stack.push('}'); }
+      // 栈为空或与栈顶元素不相同为无效字符串
+      else if stack.is_empty() || chars[i] != stack.pop().unwrap() { return false; }
+    }
+    // 匹配结束，栈为空代表是有效字符串，否则是无效字符串
+    return stack.is_empty();
+  }
+}
+
+impl Solution {
+  pub fn is_valid(s: String) -> bool {
     let s: Vec<char> = s.chars().collect();
     let mut stack = vec![];
     for i in 0..s.len() {
