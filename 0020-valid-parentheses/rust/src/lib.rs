2a0383613325d03d83e9a2ed7cfd325136d4f31b
Custer <custertian@163.com>
2021-08-04 21:25:54 +0800

    0020-valid-parentheses

--- 0020-valid-parentheses/rust/src/lib.rs
+++ 0020-valid-parentheses/rust/src/lib.rs
@@ -2,6 +2,60 @@ struct Solution;
 
 impl Solution {
   pub fn is_valid(s: String) -> bool {
+    // 当字符串长度为奇数的时候，属于无效情况，直接返回 false
+    if s.len() % 2 == 1 {
+      // 无效情况，返回 false
+      return false;
+    }
+
+    // 构建一个栈，用来存储括号
+    let  mut stack: Vec<char> = Vec::new();
+    // 把字符串装换为数组的形式，方便获取字符串中的每个字符
+    let char_array: Vec<char> = s.chars().collect();
+
+    // 遍历字符串数组中的所有元素
+    for i in 0..char_array.len() {
+      // 获取此时的字符
+      let c = char_array[i];
+      // 如果字符为左括号 (, 那么就在栈中添加对左括号
+      if c == '(' {
+        // 添加对应左括号 (
+        stack.push('(');
+        // 如果字符为左括号 [, 那么就在栈中添加对左括号 [
+      } else if c == '[' {
+        // 添加对应的左括号 [
+        stack.push('[');
+        // 如果字符为左括号 {, 那么就在栈中添加对左括号 {
+      } else if c == '{' {
+        // 添加对应左括号 {
+        stack.push('{');
+        // 否则的话，说明此时 c 是 ) ] } 这三种符号的一种
+      } else {
+        // 如果栈已经为空，而现在遍历的字符 c 是 ) ] } 这三种符号的一种
+        // 找不到可以匹配的括号，返回 false
+        // 比如这种情况 }{, 直接从右括号开始，此时栈为空
+        if stack.is_empty() { return false; }
+        // 如果栈不为空，获取栈顶元素
+        let top = stack[stack.len()-1];
+        // 将栈顶元素和此时的元素 c 进行比较，如果可以发生闭合，则将栈顶元素移除
+        if top == '(' && c == ')' || top == '[' && c == ']' || top =='{' && c =='}' {
+          // 移除栈顶元素
+          stack.pop();
+        } else {
+          // 如果不相同，说明不匹配，返回 false
+          return false;
+        }
+      }
+    }
+    // 遍历完整字符数组，判断栈是否为空
+    // 如果栈为空，说明字符数组中的所有括号都是闭合的
+    // 如果栈不为空，说明有未闭合的括号
+    stack.is_empty()
+  }
+}
+
+impl Solution {
+  pub fn is_valid(s: String) -> bool {
     let chars: Vec<char> = s.chars().collect();
     // 判断是否为空字符串，空字符串视为有效字符串
     if chars.len() == 0 { return true; }
