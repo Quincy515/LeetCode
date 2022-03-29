70a5912a7c39bc74a71101cd8c4be108ce2eda4e
CusterFun <custer@email.cn>
2022-03-29 17:02:57 +0800

    0063-unique-paths-ii

---
+++ 0063-unique-paths-ii/javascript/Solution.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    let m = obstacleGrid.length
+    let n = obstacleGrid[0].length
+    let dp = Array(m).fill().map(() => Array(n).fill(0))
+    if (obstacleGrid[0][0] == 1) {
+        dp[0][0] = 0
+    } else {
+        dp[0][0] = 1
+    }
+    for (let j = 1; j < n; ++j) {
+        if (obstacleGrid[0][j] == 1) {
+            dp[0][j] = 0
+        } else {
+            dp[0][j] = dp[0][j-1]
+        }
+    }
+    for (let i = 1; i < m; ++i) {
+        if (obstacleGrid[i][0] == 1) {
+            dp[i][0] = 0
+        } else {
+            dp[i][0] = dp[i-1][0]
+        }
+    }
+    for (let i = 1; i < m; ++i) {
+        for (let j = 1; j < n; ++j) {
+            if (obstacleGrid[i][j] == 1) {
+                dp[i][j] = 0
+            } else {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            }
+        }
+    }
+    return dp[m-1][n-1]
+};
